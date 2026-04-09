11663a12b430a4deb70d7724a2e64eca193a6c05
PatilVaibhavi-11 <vaibhavipa02@gmail.com>
2026-04-09 15:58:04 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,8 @@
 import unittest
-from app import add_numbers, divide_numbers
+from app import  divide_numbers
 
 
 class TestApp(unittest.TestCase):
-
-    def test_add(self):
-        self.assertEqual(add_numbers(2, 3), 5)
 
     def test_divide(self):
         self.assertEqual(divide_numbers(10, 2), 5)
