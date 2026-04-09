54408d2152175d0f6d8f24490e854deed1883660
PatilVaibhavi-11 <vaibhavipa02@gmail.com>
2026-04-09 15:58:45 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,11 @@
 import unittest
-from app import  divide_numbers
+from app import  divide_number
 
 
 class TestApp(unittest.TestCase):
 
     def test_divide(self):
-        self.assertEqual(divide_numbers(10, 2), 5)
+        self.assertEqual(divide_number(10, 2), 5)
 
 
 if __name__ == "__main__":
